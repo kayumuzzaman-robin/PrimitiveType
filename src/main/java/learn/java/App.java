ee798a174f23c5a1bedcd6686f2b93793d0b6e23
kayumuzzaman <kayumuzzaman.robin@gmail.com>
2021-04-06 10:26:48 +0600

    .gitignore added & modified

--- src/main/java/learn/java/App.java
+++ src/main/java/learn/java/App.java
@@ -2,12 +2,19 @@ package learn.java;
 
 /**
  * Primitive Type
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Primitive Type" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Primitive Type");
+        int a = 10; // here 10 is a literal
+        int b = a * a; // here this is an expression, not literal
+ //       will cause error, need to cast to short as java by default cast to int &
+//        int can't store in short
+
+//        short c = a * a;
+        short c = (short) (a * a);
+
+        System.out.println("a = " + a + " & b = " + b + " & c = " + c);
+
     }
 }
