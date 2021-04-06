55bdaeb95764f25041b79ef8cac299178916c50c
kayumuzzaman <kayumuzzaman.robin@gmail.com>
2021-04-06 10:15:43 +0600

    Initial commit

---
+++ src/main/java/learn/java/App.java
@@ -0,0 +1,13 @@
+package learn.java;
+
+/**
+ * Primitive Type
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Primitive Type" );
+    }
+}
